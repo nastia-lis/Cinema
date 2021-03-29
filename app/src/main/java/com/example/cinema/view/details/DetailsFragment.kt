33447b654ad3765c8a4d1ddc1dc45da6a11ebc3f
Nastia <nastiusha_super@mail.ru>
2021-03-29 10:20:16 +0300

    hw7

--- app/src/main/java/com/example/cinema/view/details/DetailsFragment.kt
+++ app/src/main/java/com/example/cinema/view/details/DetailsFragment.kt
@@ -15,18 +15,22 @@ import com.example.cinema.databinding.FragmentDetailsBinding
 import com.example.cinema.model.Cinema
 import com.example.cinema.model.MovieDTO
 import com.example.cinema.model.Movie
+import com.example.cinema.viewmodel.AppState
 import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_details.*
 import kotlinx.android.synthetic.main.fragment_main_recycler_item.nameMovie
 import kotlinx.android.synthetic.main.fragment_main_recycler_item.rating
 import kotlinx.android.synthetic.main.fragment_main_recycler_item.released
+import okhttp3.*
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
 import java.net.MalformedURLException
 import java.net.URL
 import java.util.stream.Collectors
 import javax.net.ssl.HttpsURLConnection
+import kotlin.jvm.Throws
 
 class DetailsFragment : Fragment() {
 
@@ -71,16 +75,59 @@ class DetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         movieBundle = arguments?.getParcelable(BUNDLE_EXTRA) ?: Movie()
-        binding.fragmentDescription.visibility = View.GONE
-        binding.loading.visibility = View.VISIBLE
-        val loader = MovieLoader(onLoaderListener, movieBundle.cinema.id)
-        loader.loadMovie()
+//        binding.fragmentDescription.visibility = View.GONE
+//        binding.loading.visibility = View.VISIBLE
+//        val loader = MovieLoader(onLoaderListener, movieBundle.cinema.id)
+//        loader.loadMovie()
+        getMovie()
     }
 
     private fun displayMovie(movieDTO: MovieDTO) {
         with(binding) {
             fragmentDescription.visibility = View.VISIBLE
             loading.visibility = View.GONE
+            nameMovie.text = movieDTO.title
+            released.text = movieDTO.release_date
+            rating.text = movieDTO.vote_average.toString()
+            description.text = movieDTO.overview
+        }
+    }
+
+    private fun getMovie() {
+        fragmentDescription.visibility = View.GONE
+        loading.visibility = View.VISIBLE
+
+        val client = OkHttpClient()
+        val builder: Request.Builder = Request.Builder()
+        builder.url("https://api.tmdb.org/3/movie/${movieBundle.cinema.id}?api_key=ba94ef13c60c86a4f3cc51f6e1cb74b5")
+        val request: Request = builder.build()
+        val call: Call = client.newCall(request)
+        call.enqueue(object : Callback {
+            val handler: Handler = Handler()
+            @Throws(IOException::class)
+            override fun onResponse(call: Call?, response: Response) {
+                val serverResponse: String? = response.body()?.string()
+                if (response.isSuccessful && serverResponse != null) {
+                    handler.post {
+                        renderData(Gson().fromJson(serverResponse, MovieDTO::class.java))
+                    }
+                } else {
+                    AppState.Error(Throwable("Error Server"))
+                }
+            }
+            override fun onFailure(call: Call?, e: IOException?) {
+                AppState.Error(Throwable("Error Server"))
+            }
+        })
+    }
+
+    private fun renderData(movieDTO: MovieDTO) {
+        fragmentDescription.visibility = View.VISIBLE
+        loading.visibility = View.GONE
+
+        if (movieDTO.title.isNullOrEmpty() || movieDTO.release_date.isNullOrEmpty() || movieDTO.vote_average == null || movieDTO.overview.isNullOrEmpty()) {
+            AppState.Error(Throwable("Error Server"))
+        } else {
             nameMovie.text = movieDTO.title
             released.text = movieDTO.release_date
             rating.text = movieDTO.vote_average.toString()
