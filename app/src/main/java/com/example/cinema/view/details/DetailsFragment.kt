add16eca23e15fdbe2ce2377e549dc1133c28cd2
Nastia <nastiusha_super@mail.ru>
2021-03-29 12:26:03 +0300

    hw7

--- app/src/main/java/com/example/cinema/view/details/DetailsFragment.kt
+++ app/src/main/java/com/example/cinema/view/details/DetailsFragment.kt
@@ -3,33 +3,22 @@ package com.example.cinema.view.details
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.RequiresApi
-import com.example.cinema.MovieLoader
 import com.example.cinema.databinding.FragmentDetailsBinding
-import com.example.cinema.model.Cinema
 import com.example.cinema.model.MovieDTO
 import com.example.cinema.model.Movie
 import com.example.cinema.viewmodel.AppState
-import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_details.*
 import kotlinx.android.synthetic.main.fragment_main_recycler_item.nameMovie
 import kotlinx.android.synthetic.main.fragment_main_recycler_item.rating
 import kotlinx.android.synthetic.main.fragment_main_recycler_item.released
 import okhttp3.*
-import java.io.BufferedReader
 import java.io.IOException
-import java.io.InputStreamReader
-import java.net.MalformedURLException
-import java.net.URL
-import java.util.stream.Collectors
-import javax.net.ssl.HttpsURLConnection
 import kotlin.jvm.Throws
 
 class DetailsFragment : Fragment() {
@@ -37,16 +26,6 @@ class DetailsFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
     private lateinit var movieBundle: Movie
-
-    private val onLoaderListener: MovieLoader.MovieLoaderListener =
-        object : MovieLoader.MovieLoaderListener {
-            override fun onLoaded(movieDTO: MovieDTO) {
-                displayMovie(movieDTO)
-            }
-
-            override fun onFailed(throwable: Throwable) {
-            }
-        }
 
     companion object {
         const val BUNDLE_EXTRA = "movie"
@@ -75,22 +54,7 @@ class DetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         movieBundle = arguments?.getParcelable(BUNDLE_EXTRA) ?: Movie()
-//        binding.fragmentDescription.visibility = View.GONE
-//        binding.loading.visibility = View.VISIBLE
-//        val loader = MovieLoader(onLoaderListener, movieBundle.cinema.id)
-//        loader.loadMovie()
         getMovie()
-    }
-
-    private fun displayMovie(movieDTO: MovieDTO) {
-        with(binding) {
-            fragmentDescription.visibility = View.VISIBLE
-            loading.visibility = View.GONE
-            nameMovie.text = movieDTO.title
-            released.text = movieDTO.release_date
-            rating.text = movieDTO.vote_average.toString()
-            description.text = movieDTO.overview
-        }
     }
 
     private fun getMovie() {
